bf25c2d87cbadd315d806bd07622ebb3ded01bcd
Maxim <m1z11987@mail.ru>
2023-02-09 13:16:00 +0500

    first-react

---
+++ ClientApp/src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <h1>123</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+
+//const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(<App />);
